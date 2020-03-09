763d38102d91e3669e43a476364ec5a69bd1981c
Erik Benoist <ebenoist@gmail.com>
2020-03-09 12:51:00 -0500

    Removes need for PR template
    Instead of providing a PR template, this action can supply
    the message in the form of a comment. This should cut down
    on boilerplate noise.
    
    Added basic testing framework.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,16 +1,109 @@
-import * as cp from 'child_process'
-import * as path from 'path'
+import * as nock from 'nock';
+nock.disableNetConnect()
 
-test('does nothing atm', () => {
-  expect(true).toBeTruthy();
+import { onPullRequest } from '../src/on_pull_request';
+import * as github from '@actions/github'
+
+nock('https://api.github.com').log(console.log)
+
+const ghClient = new github.GitHub('foozles');
+const input = {
+  slackHook: 'https://foo.slack/hook',
+  instructions: 'this is how we pr',
+  skipApprovalLabel: 'emergency-approval',
+  skipCILabel: 'emergency-ci',
+}
+
+describe('pull request actions', () => {
+  afterEach(() => {
+    expect(nock.isDone());
+  });
+
+  test('on open', async () => {
+    let body;
+
+    nock('https://api.github.com')
+      .post('/repos/github/my-repo/issues/12/comments', (req) =>  {
+        body = req;
+        return true;
+      }).reply(200, 'way to go');
+
+    await onPullRequest(
+      ghClient,
+      {
+        payload: { action: 'opened' },
+        issue: { owner: 'github', repo: 'my-repo', number: 12 },
+        ref: 'f123',
+      },
+      input,
+    );
+
+    expect(body).toEqual({ body: 'this is how we pr' });
+  });
+
+  describe('on label', () => {
+    let slackMsg;
+
+    beforeEach(() => {
+      nock('https://foo.slack/')
+        .post('/hook', (req) =>  {
+          slackMsg = req;
+          return true;
+        }).reply(200, 'way to go');
+    });
+
+    test('on label emergency-approval', async () => {
+      let ghReviewBody;
+
+      const ghApprovalReq = nock('https://api.github.com/')
+        .post('/repos/github/my-repo/pulls/12/reviews', (req) =>  {
+          ghReviewBody = req;
+          return true;
+        }).reply(200, 'way to go');
+
+      await onPullRequest(
+        ghClient,
+        {
+          payload: { action: 'labeled', label: { name: 'emergency-approval' } },
+          issue: { owner: 'github', repo: 'my-repo', number: 12 },
+          ref: 'f123',
+        },
+        input,
+      );
+
+      expect(ghReviewBody).toEqual({
+        body: 'Skipping approval check - emergency-approval applied',
+        event: 'APPROVE',
+      });
+    });
+
+    test('on label emergency-ci', async () => {
+      let ghCommentBody;
+
+      const ghCommentReq = nock('https://api.github.com')
+        .post('/repos/github/my-repo/issues/12/comments', (req) =>  {
+          ghCommentBody = req;
+          return true;
+        }).reply(200, 'way to go');
+
+      const ghChecksReq = nock('https://api.github.com')
+        .get('/repos/github/my-repo/commits/f123/check-runs')
+        .reply(200, 'way to go');
+
+      await onPullRequest(
+        ghClient,
+        {
+          payload: { action: 'labeled', label: { name: 'emergency-ci' } },
+          issue: { owner: 'github', repo: 'my-repo', number: 12 },
+          ref: 'f123',
+        },
+        input,
+      );
+
+      expect(ghCommentBody).toEqual({ body: 'Bypassing CI checks - emergency-ci applied' });
+      expect(slackMsg).toEqual({
+        text: 'Bypassing CI checks for: https://github.com/github/my-repo/12'
+      });
+    });
+  });
 });
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
-})
